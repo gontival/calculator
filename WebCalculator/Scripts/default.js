4d9b53fb5a9cd5c50468a945363b18e8102aa7bf
MCNEXT\jlevant <jlevant@mcnext.com>
2017-02-14 17:09:14 +0100

    Working with forms, lesson 1 & 2 Understanding Forms and form validation

---
+++ WebCalculator/Scripts/default.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="_references.js" /> 
+
+$(document).ready(function () {
+    $('#myButton').on('click', submitTheForm);
+});
+
+function submitTheForm() {
+    $('#myForm').submit();
+
+    //var formData = $('#myForm').serialize();
+    //var data = decodeURIComponent(formData);
+
+}
+
+//$(document).ready(function () {
+//    $('input[name="comment"]').focus();
+//});
